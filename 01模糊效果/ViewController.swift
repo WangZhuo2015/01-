d4bfccb2e27dd5a60444274493ebcfbfa3796819
王卓 <wangzhuo@wangzhuodeMacBook-Pro.local>
2015-07-22 23:36:29 +0800

    模糊效果Demo
    模糊效果Demo

--- 01模糊效果/ViewController.swift
+++ 01模糊效果/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var BackGround: UIImageView!
+    @IBOutlet weak var Icon: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        //设置圆角
+        Icon.layer.cornerRadius=100
+        Icon.clipsToBounds=true
+        
+        //实例化模糊效果类，参数为风格
+        let blurEffert=UIBlurEffect(style: UIBlurEffectStyle.ExtraLight)
+        //实例化一个View
+        let EffertView=UIVisualEffectView(effect: blurEffert)
+        EffertView.alpha=0.6
+        //设置view的大小
+        EffertView.frame.size=CGSize(width: view.frame.width, height: view.frame.height)
+        //加入视图
+        BackGround.addSubview(EffertView)
 
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
